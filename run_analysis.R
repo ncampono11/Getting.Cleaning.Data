4fc38473032cc6e5241033bb39d1125f76c76c5d
ncampono11 <64989354+ncampono11@users.noreply.github.com>
2020-06-29 10:51:26 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,74 +1,74 @@
 # Peer Assignment 1
-library(dplyr)
-
-getwd()
-setwd("C:/Users/ncampono/Desktop/R/Courses/Getting Cleaning Data/R/Getting_Cleaning_Data/Peer_Assignment")
 
 
-features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+#Download, Read and Convert Data
 
-  # Step 1: Merges the training and the test sets to create one data set.
+library(data.table)
+fileurl = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+if (!file.exists('./UCI HAR Dataset.zip')){
+  download.file(fileurl,'./UCI HAR Dataset.zip', mode = 'wb')
+  unzip("UCI HAR Dataset.zip", exdir = getwd())
+}
 
-ds.x <- rbind(x_train, x_test)
-ds.y <- rbind(y_train, y_test)
-subject <- rbind(subject_train, subject_test)
 
-uci.df <- cbind(subject, ds.x, ds.y)
+features <- read.csv('./UCI HAR Dataset/features.txt', header = FALSE, sep = ' ')
+features <- as.character(features[,2])
 
-uci.df
+train.x <- read.table('./UCI HAR Dataset/train/X_train.txt')
+train.activity <- read.csv('./UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = ' ')
+train.subject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep = ' ')
+
+data.train <-  data.frame(train.subject, train.activity, train.x)
+names(data.train) <- c(c('subject', 'activity'), features)
+
+test.x <- read.table('./UCI HAR Dataset/test/X_test.txt')
+test.activity <- read.csv('./UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = ' ')
+test.subject <- read.csv('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
+
+data.test <-  data.frame(test.subject, test.activity, test.x)
+names(data.test) <- c(c('subject', 'activity'), features)
+
+
+
+# Step 1: Merges the training and the test sets to create one data set.
+
+uci <- rbind(data.train, data.test)
 
 # Step 2: Extracts only the measurements on the mean and standard deviation for each measurement
 
-
-uci.mstd <- uci.df %>% select(subject, code, contains("mean"), contains("std"))
-
-uci.mstd
+mean.std.select <- grep('mean|std', features)
+uci.mstd <- uci[,c(1,2,mean.std.select + 2)]
 
 # Step 3: Uses descriptive activity names to name the activities in the data set
 
-uci.mstd$code<- activities[uci.mstd$code, 2]
-
-uci.mstd$code
+activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+activity.labels <- as.character(activity.labels[,2])
+uci.mstd$activity <- activity.labels[uci.mstd$activity]
 
 # Step 4: Appropriately labels the data set with descriptive variable names.
 
 
-names(uci.mstd)
-
-names(uci.mstd)[2] = "activity"
-names(uci.mstd)<-gsub("Acc", "accelerometer", names(uci.mstd))
-names(uci.mstd)<-gsub("Gyro", "gyroscope", names(uci.mstd))
-names(uci.mstd)<-gsub("BodyBody", "body", names(uci.mstd))
-names(uci.mstd)<-gsub("Mag", "magnitude", names(uci.mstd))
-names(uci.mstd)<-gsub("^t", "time", names(uci.mstd))
-names(uci.mstd)<-gsub("^f", "frequency", names(uci.mstd))
-names(uci.mstd)<-gsub("tBody", "timebody", names(uci.mstd))
-names(uci.mstd)<-gsub("-mean()", "mean", names(uci.mstd), ignore.case = TRUE)
-names(uci.mstd)<-gsub("-std()", "STD", names(uci.mstd), ignore.case = TRUE)
-names(uci.mstd)<-gsub("-freq()", "frequency", names(uci.mstd), ignore.case = TRUE)
-names(uci.mstd)<-gsub("angle", "angle", names(uci.mstd))
-names(uci.mstd)<-gsub("gravity", "gravity", names(uci.mstd))
-
-names(uci.mstd)
+name.new <- names(uci.mstd)
+name.new <- gsub("[(][)]", "", name.new)
+name.new <- gsub("^t", "TimeDomain_", name.new)
+name.new <- gsub("^f", "FrequencyDomain_", name.new)
+name.new <- gsub("Acc", "Accelerometer", name.new)
+name.new <- gsub("Gyro", "Gyroscope", name.new)
+name.new <- gsub("Mag", "Magnitude", name.new)
+name.new <- gsub("-mean-", "_Mean_", name.new)
+name.new <- gsub("-std-", "_StandardDeviation_", name.new)
+name.new <- gsub("-", "_", name.new)
+names(uci.mstd) <- name.new
 
 
 #Step 5: From the data set in step 4, creates a second, 
 #independent tidy data set with the average of each variable for each activity and each subject.
 
 
-uci.final <- uci2 %>%
-  group_by(subject, activity) %>%
-  summarise_all(mean)
+tidy.data <- aggregate(uci.mstd[,3:81], by = list(activity = uci.mstd$activity, subject = uci.mstd$subject),FUN = mean)
+write.table(x = tidy.data, file = "tidy_data.txt", row.names = FALSE)
 
-uci.final
 
-str(uci.final)
 
-write.table(uci.final, "uci.final.txt", row.name=FALSE)
+
+
